3a612998f5904036ea4e528c37970113c9d59c6b
Derek Garnett <rizzagum@gmail.com>
2024-07-10 21:31:59 +0000

    FEAT adds to undex functiion

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    if request.method == "POST":
+        return HttpResponse("You POSTED something")
+    elif request.method == "GET":
+        return HttpResponse(request.method)
